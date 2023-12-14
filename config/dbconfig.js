1c8d096a9af25f1254f70609ddee038161c8f3a2
Razz-10 <razzleo248@gmail.com>
2023-12-14 19:31:15 +0545

    CMS

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,13 +1,28 @@
+//local host
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "",
+//     DB: "CMS",
+//     dialect: "mysql",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000,
+//     },
+//   };
+
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "CMS",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  };
+      HOST: "viaduct.proxy.rlwy.net",
+      USER: "root",
+      PASSWORD: "hcG-GHF-251aFB56-CbdDf6d2H5EAfd2",
+      DB: "railway",
+      dialect: "mysql",
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
+    };
